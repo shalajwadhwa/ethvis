e0d88561106cbd44509df5a46a7030fdbf3b24e8
Shalaj Wadhwa <48180546+shalajwadhwa@users.noreply.github.com>
2025-01-31 21:38:55 +0000

    Fix issue where untested files not covered

--- jest.config.ts
+++ jest.config.ts
@@ -28,7 +28,11 @@ const config: Config = {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: [
+    "app/**/*.{ts,tsx}",
+    "!app/**/*.d.ts",
+    "!app/**/__tests__/**"
+  ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
