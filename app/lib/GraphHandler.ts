87b62893926de2f41afddb8dc40f4b7a64d81f43
Shalaj Wadhwa <48180546+shalajwadhwa@users.noreply.github.com>
2025-02-07 04:46:36 +0000

    Add logic to update node colours

--- app/lib/GraphHandler.ts
+++ app/lib/GraphHandler.ts
@@ -2,22 +2,6 @@ import Graph from 'graphology';
 import { Transaction } from '@/app/types/transaction';
 import Sigma from 'sigma';
 
-export function updateNodeColour(graph: Graph, tx: Transaction, netBalanceTo: number, netBalanceFrom: number): Graph {
-  if (netBalanceTo > 0) {
-    graph.setNodeAttribute(tx.to, 'color', 'green');
-  } else {
-    graph.setNodeAttribute(tx.to, 'color', 'red');
-  }
-
-  if (netBalanceFrom > 0) {
-    graph.setNodeAttribute(tx.from, 'color', 'green');
-  } else {
-    graph.setNodeAttribute(tx.from, 'color', 'red');
-  }
-
-  return graph;
-}
-
 type NodeType = { x: number; y: number; label: string; size: number };
 type EdgeType = { label: string };
 
@@ -27,17 +11,41 @@ class GraphHandler {
     if (!graph) {
       return;
     }
+    console.log("Size: ", graph.order);
 
     const x = graph.order;
 
     if (!graph.hasNode(tx.from)) {
-      graph.addNode(tx.from, { label: x, x: Math.random(), y: Math.random() });
+      graph.addNode(tx.from, { label: x, x: Math.random(), y: Math.random(), size: 4 });
     }
     if (!graph.hasNode(tx.to)) {
-      graph.addNode(tx.to, { label: x+1, x: Math.random(), y: Math.random() });
+      graph.addNode(tx.to, { label: x+1, x: Math.random(), y: Math.random(), size: 4 });
     }
     if (!graph.hasEdge(tx.from, tx.to)) {
       graph.addEdge(tx.from, tx.to);
+    }
+  }
+
+  public static updateNodeColour(sigma: Sigma<NodeType, EdgeType>, tx: Transaction, netBalance: number, is_sender: boolean): void {
+    const graph: Graph = sigma.getGraph();
+    if (!graph) {
+      return;
+    }
+
+    const node = is_sender ? tx.from : tx.to;
+
+    if (!graph.hasNode(node)) {
+      return;
+    }
+
+    if (netBalance > 0) {
+      graph.setNodeAttribute(node, 'color', 'green');
+    } 
+    else if (netBalance < 0) {
+      graph.setNodeAttribute(node, 'color', 'red');
+    }
+    else {
+      return;
     }
   }
 }
