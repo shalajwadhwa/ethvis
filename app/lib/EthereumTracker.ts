def8d8a789bfd827e173e6604ed54f8fce29017b
Shalaj Wadhwa <48180546+shalajwadhwa@users.noreply.github.com>
2025-02-22 19:10:18 +0000

    Refector variable names

--- app/lib/EthereumTracker.ts
+++ app/lib/EthereumTracker.ts
@@ -4,11 +4,12 @@ import { EventType } from '@/app/types/event';
 import EthereumApiClient from "@/app/lib/EthereumApiClient";
 import { AddressInfoResponse } from '@/app/types/graph';
 
+const MAX_MEMPOOL_SIZE = 2000;
+
 class EthereumTracker {
     private static instance: EthereumTracker;
-    private net_balance: Map<string, number> = new Map();
+    private netBalance: Map<string, number> = new Map();
     private mempool: Transaction[] = [];
-    private max_mempool_size: number = 2000;
 
     public static getInstance(): EthereumTracker {
         if (!EthereumTracker.instance) {
@@ -79,14 +80,14 @@ class EthereumTracker {
     }
 
     public async addPendingTransaction(tx: Transaction) {
-        if (this.mempool.length >= this.max_mempool_size) {
+        if (this.mempool.length >= MAX_MEMPOOL_SIZE) {
             this.shiftMempool();
         }
 
-        if (!this.net_balance.has(tx.from)) {
+        if (!this.netBalance.has(tx.from)) {
             await this.addNewAddress(tx.from);
         }
-        if (!this.net_balance.has(tx.to)) {
+        if (!this.netBalance.has(tx.to)) {
             await this.addNewAddress(tx.to, true);
         }
 
@@ -96,8 +97,8 @@ class EthereumTracker {
     public updateNetBalance(tx: Transaction, value: number, is_sender: boolean) {
         const node = is_sender ? tx.from : tx.to;
 
-        const prev_balance = this.net_balance.get(node) || 0;
-        this.net_balance.set(node, prev_balance + value);
+        const prev_balance = this.netBalance.get(node) || 0;
+        this.netBalance.set(node, prev_balance + value);
         eventEmitter.emit(EventType.UpdateNodeNetBalance, tx, value, is_sender);
     }
 
