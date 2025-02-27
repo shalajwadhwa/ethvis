2c48ae8b23fb0bb949e836a1b00fc36d8c76a7b9
Shalaj Wadhwa <48180546+shalajwadhwa@users.noreply.github.com>
2025-02-27 22:14:22 +0000

    Revert transaction type

--- app/types/transaction.ts
+++ app/types/transaction.ts
@@ -1,3 +1,16 @@
-import { TransactionReceipt } from "alchemy-sdk";
-
-export type Transaction = TransactionReceipt;
+export interface Transaction {
+    hash: string;
+    nonce: string;
+    blockHash?: string;
+    blockNumber?: string;
+    transactionIndex?: string;
+    from: string;
+    to: string;
+    value: string;
+    gasPrice: string;
+    gas: string;
+    input: string;
+    v?: string;
+    r?: string;
+    s?: string;
+}
